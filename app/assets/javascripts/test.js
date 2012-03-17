debc86250aa4bdc408d077a19f2733526485be10
Ken Orji <kokenjr@gmail.com>
2012-03-17 04:13:08 -0400

    added hover popup

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,19 @@
+//$(document).ready(function() {
+//    $(".record-each").each(function(index) {
+//      $(this).hover(function() {
+//        $(this).find('.popup').show();
+//      }, function() {
+//        $('.popup').hide();
+//      });
+//      });
+//});
+
+//$(document).ready(function() {
+//  $(".record-each").hover(function(i) {
+//    $('#record-id1').hover(function() {
+//        div$('#popup-1').show();
+//    }, function() {
+//        $('#popup-1').hide();
+//    });
+//  });
+//});
