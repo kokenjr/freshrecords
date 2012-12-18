6522188514bfd0f62e31a7f7a725307b79b4bc78
Ken Orji <kokenjr@gmail.com>
2012-12-18 00:47:16 -0500

    refactoring javascript

---
+++ app/assets/javascripts/popup.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".record-each").each(function(index) {
+      $(this).hover(function() {
+        $(this).find('.popup').show();
+      }, function() {
+        $('.popup').hide();
+      }); 
+    }); 
+});
